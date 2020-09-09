e8a8fbf7d9e52364497b162aba699266e5cfb147
AlexandrSarzhan <sarzhanalexandr@yahoo.com>
2020-09-09 14:23:11 +0300

    remove /favicon.ico rout

--- src/Handler/Common.hs
+++ src/Handler/Common.hs
@@ -10,12 +10,6 @@ import Data.Aeson as A
 -- These handlers embed files in the executable at compile time to avoid a
 -- runtime dependency, and for efficiency.
 
-getFaviconR :: Handler TypedContent
-getFaviconR = do cacheSeconds $ 60 * 5
-                 --cacheSeconds $ 60 * 60 * 24 * 30 -- cache for a month
-                 return $ TypedContent "image/x-icon"
-                        $ toContent $(embedFile "config/favicon.ico")
-
 getRobotsR :: Handler TypedContent
 getRobotsR = return $ TypedContent typePlain
                     $ toContent $(embedFile "config/robots.txt")
